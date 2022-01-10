1ea2a3073e4f62df5ef97cc9420e7676d60b7b40
Unnati <chhabraunnati324@gmail.com>
2022-01-10 22:22:48 +0530

    added the files

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
